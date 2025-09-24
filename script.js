9744fa62b86c7158a5c998a2f2a472537bc91c0f
Ross Whitehouse <ross@MacBookAir.lan>
2025-09-24 15:20:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const canvasEl = document.getElementById('canvas')
+const canvas = canvasEl.getContext('2d')
+const defaultCanvasHeight = document.getElementById('canvas').height
+
+function setCanvas(height = defaultCanvasHeight) {
+    canvasEl.height = height
+    canvas.textAlign = 'center'
+    canvas.font = 'bold 16px Tahoma, Helvetica, sans-serif'
+    canvas.fillStyle = 'white'
+}
+
+function updateCanvas(event) {
+    event.preventDefault()
+    canvas.clearRect(0, 0, 300, 500)
+
+    // Title
+    canvas.textAlign = 'center'
+    canvas.font = 'bold 16px Tahoma, Helvetica, sans-serif'
+    canvas.fillStyle = 'white'
+    const string =  document.getElementById('name').value
+    const strLen = string.length
+    let strLines = []
+    let newLine = ''
+    if (strLen <= 35) {
+        setCanvas()
+        canvas.fillText(document.getElementById('name').value, 150, 430)
+    } else {
+        string.split(' ').forEach(word => {
+            // if it would be too long, start a new line
+            if ((newLine.length + word.length + 1) > 35) {
+                strLines.push(newLine)
+                newLine = word
+            } else {
+                newLine === '' ? newLine += word : newLine += ' ' + word
+            }
+        })
+        strLines.push(newLine)
+        setCanvas(defaultCanvasHeight + (8 * strLines.length))
+        strLines.forEach((line, i) => {
+            canvas.fillText(line, 150, 430 + (i*16))
+        })
+    }
+
+    // Image
+    const img = new Image(200, 300)
+    img.src = document.getElementById('img').value
+    img.onload = function() {
+        canvas.drawImage(img, 25, 0, 250, 400)
+    }
+
+    // Stars
+    const stars = '⭐️'.repeat(parseInt(document.getElementById('rating').value))
+    const top = !strLines.length ? 460 : 460 + ((strLines.length - 1) * 16)
+    canvas.fillText(stars, 150, top)
+
+    if (!!document.getElementById('img').value && !!string) {
+        const canvasUrl = canvasEl.toDataURL()
+        const button = document.querySelector('a.download')
+        button.href = canvasUrl
+        button.download = 'letterboxd_img'
+        button.classList.remove('disabled')
+    }
+}
+
+document.querySelector('button[type="submit"]').addEventListener('click', updateCanvas)
